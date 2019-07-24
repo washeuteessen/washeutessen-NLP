1620aa3cb0523c861ee98ceb84c77690193dfa4b
Magdalena <mdeschner@hotmail.de>
2019-07-24 19:03:13 +0200

    adapt file names

--- Match_ingredients/match_ingredients_pipeline.py
+++ Match_ingredients/match_ingredients_pipeline.py
@@ -1,44 +1,8 @@
 import spacy
-from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.pipeline import Pipeline
+from match_ingredients_pipeline import SpacyLemmatizer
 
-class SpacyLemmatizer(BaseEstimator, TransformerMixin):
-    """Class to call spacy nlp pipeline and extract lemma for each word
-    
-    Parameters
-    ----------
-    BaseEstimator : sklearn base class
-    TransformerMixin : sklearn base class       
-    """
-    def __init__(self):
-        self.nlp = spacy.load("de_core_news_md")
-
-    def fit(self, X, y=None):
-        pass
-
-    def transform(self, X):
-        """Function to retrieve lemma for each word. 
-        
-        Parameters
-        ----------
-        X : df
-            Dataframe with word in first column
-        
-        Returns
-        -------
-        dict
-            Dataframe with word in first column and corresponding lemma in second column.
-        """
-        # iterate over dataframe
-        for idx, row in X.iterrows():
-
-            # apply spacy nlp pipeline on word
-            doc = self.nlp(row[0])
-
-            # retrieve lemma for each token
-            for token in doc:
-                lemma = token.lemma_
-
-            # write lemma to df
-            X.iloc[idx,1] = lemma
-
-        return X
+pipeline_match = Pipeline([
+    ("Lemmatizer", SpacyLemmatizer()),
+    #("WordEmbedder", WordEmbedder())
+])
